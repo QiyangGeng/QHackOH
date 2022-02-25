4fa98f0d70e4d63a18c828713ef0d28c403a7416
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2022-02-25 13:53:56 -0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,47 @@
+import numpy as np
+import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
+from qiskit.quantum_info.operators import Operator
+from qiskit.ignis.verification.tomography import state_tomography_circuits, StateTomographyFitter
+import qiskit.quantum_info as qi
+
+# bus = ClassicalRegister(1)
+quantumReg = QuantumRegister(3)
+classicReg = ClassicalRegister(3)
+circuit = QuantumCircuit(quantumReg, classicReg)
+
+# Input
+
+
+# Bell state
+circuit.x(2)
+Matrix = Operator([
+    [1, 0, 0, 0],
+    [0, 1/np.sqrt(2), 1j/np.sqrt(2), 0],
+    [0, 1j/np.sqrt(2), 1/np.sqrt(2), 0],
+    [0, 0, 0, 1]])
+circuit.unitary(Matrix, [0, 1])
+QuantumCircuit.decompose(circuit)
+circuit.rz(np.pi/2, 2)
+
+# Cloning
+
+
+# Measurement
+circuit.h(quantumReg[0])
+for j in range(2):
+    circuit.cx(quantumReg[j], quantumReg[j + 1])
+
+# Get ideal output state
+target_state_bell5 = qi.Statevector.from_instruction(circuit)
+
+# Generate circuits and run on simulator
+qst_bell5 = state_tomography_circuits(circuit, quantumReg)
+job = qiskit.execute(qst_bell5, Aer.get_backend('qasm_simulator'), shots=5000)
+
+# Extract tomography data so that counts are indexed by measurement configuration
+tomo_bell5 = StateTomographyFitter(job.result(), qst_bell5)
+
+circuit.measure(0, 0)
+circuit.measure(1, 1)
+circuit.measure(2, 2)
